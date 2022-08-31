4cb2fb10c9a56bc60ebbebe4c0e0e052f099ad83
Hannah Karlsson <gamemod.mod@gmail.com>
2022-08-31 14:55:48 +0200

    Starten på programmet.
    Fungerar absolut inte, ser dock snyggt ut!

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Lab i enlighet med följande:
+https://github.com/niklas-hjelm/Programmering-med-C-Sharp-NET22/blob/master/assets/Labb%201.md
+
+Skapat av Hannah Karlsson för ITHS .NET 22
+
+*/
+var delStrang = "29535123p48723487597645723645";
+
+for(int i = 0; i < delStrang.Length; i++)
+{
+    if (int.TryParse(delStrang[i].ToString(), out int result)) {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(delStrang[i]);
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write(delStrang[i]);
+    }
+}
+Console.ResetColor();
